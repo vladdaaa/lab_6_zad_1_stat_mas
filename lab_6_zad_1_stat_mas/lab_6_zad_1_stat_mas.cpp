d9b7aa8b407c4b1a442f90c095a1bbb427d98bc2
vladdaaa <144156817+vladdaaa@users.noreply.github.com>
2023-12-04 22:40:02 +0700

    Добавьте файлы проекта.

---
+++ lab_6_zad_1_stat_mas/lab_6_zad_1_stat_mas.cpp
@@ -0,0 +1,52 @@
+﻿/*Даны целые положительные числа M, N и набор из N чисел.
+ Сформировать матрицу размера M × N, у которой в каждой строке содержатся все
+ числа из исходного набора (в том же порядке),каждая следующая строка умноженные на d.
+ статический*/
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int N, M;
+    int d;
+    const size_t MAX_N = 10;
+    const size_t MAX_M = 10;
+    int array[MAX_M][MAX_N];
+
+    cout << "Введите d: ";
+    cin >> d;
+
+    cout << "Введите размер матрицы M*N:\n";
+    cin >> M;
+    if (M == 0 || M > MAX_M) {
+        cerr << "Ошибка: количество должно находиться в диапазоне [1;" << MAX_M << " ]" << endl;
+        return 1;
+    }
+
+    cin >> N;
+    if (N == 0 || N > MAX_N) {
+        cerr << "Ошибка: количество должно находиться в диапазоне [1;" << MAX_N << " ]" << endl;
+        return 1;
+    }
+
+    cout << "Введите набор из " << N << " чисел: ";
+    for (int i = 0; i < N; i++)
+        cin >> array[0][i];
+    for (int j = 1; j < M; j++)
+        for (int i = 0; i < N; i++)
+            array[j][i] = array[j - 1][i]*d;
+    for (int j = 0; j < M; j++)
+    {
+        for (int i = 0; i < N; i++)
+            cout << array[j][i] << " ";
+        cout << "\n";
+
+
+    }
+
+
+}
+
+
